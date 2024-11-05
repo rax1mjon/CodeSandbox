1331c0b5ab7d1667c6bcd838df0a7c47aa91c7e8
rax1mjon <tulaganovraximjon65@gmail.com>
2024-11-05 10:51:39 +0500

    loading

--- js/main.js
+++ js/main.js
@@ -107,5 +107,22 @@ window.addEventListener("click", function (event) {
     }
 });
 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
 
 AOS.init();
